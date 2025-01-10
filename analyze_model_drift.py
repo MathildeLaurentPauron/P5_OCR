0f978e53128e8bbf365bc1182163d0f82b7221d4
Mathilde Laurent Pauron <mathilde.pauron@gmail.com>
2025-01-10 15:19:14 +0100

    creation data drift report and correction ci.yml

--- analyze_model_drift.py
+++ analyze_model_drift.py
@@ -2,6 +2,8 @@ import pandas as pd
 from evidently.report import Report
 from evidently.metric_preset import DataDriftPreset
 
+
+#initial report
 data = pd.read_pickle(r"C:\Users\mathi\OneDrive\Desktop\P5_OCR\data\data.pkl")
 
 data_drift_report = Report(metrics=[
@@ -12,3 +14,35 @@ data_drift_report.run(current_data=data.iloc[:60], reference_data=data.iloc[60:]
 data_drift_report
 
 data_drift_report.save_html("data_drif_report.html")
+
+#first section report on 2022 datas
+data_1 = pd.read_pickle(r"C:\Users\mathi\OneDrive\Desktop\P5_OCR\data\data_report_drift.pkl")
+
+data_drift_report_1 = Report(metrics=[
+    DataDriftPreset(),
+])
+
+data_drift_report_1.run(current_data=data_1.iloc[:60], reference_data=data.iloc[:60], column_mapping=None)
+data_drift_report_1
+
+data_drift_report_1.save_html("data_drif_report_1.html")
+
+#second report on 2022 datas, second section
+data_drift_report_2 = Report(metrics=[
+    DataDriftPreset(),
+])
+
+data_drift_report_2.run(current_data=data_1.iloc[1500:1560], reference_data=data.iloc[:60], column_mapping=None)
+data_drift_report_2
+
+data_drift_report_2.save_html("data_drif_report_2.html")
+
+#third report on 2022 datas, third section
+data_drift_report_3 = Report(metrics=[
+    DataDriftPreset(),
+])
+
+data_drift_report_3.run(current_data=data_1.iloc[:60], reference_data=data.iloc[:60], column_mapping=None)
+data_drift_report_3
+
+data_drift_report_3.save_html("data_drif_report_2.html")
--- test_predict_tags.py
+++ test_predict_tags.py
@@ -3,8 +3,8 @@ from utils import predict_tags
 
 class TestPredictTags(unittest.TestCase):
     def test_prediction(self):
-        input_data = "Is it possible to convert a java code in c#"
-        expected_tags = set(["java", "c#"])
+        input_data = "I have a problem with a pandas dataframe and pandas series"
+        expected_tags = set(["python", "pandas"])
         result = set(predict_tags(input_data)[0])
         self.assertEqual(result, expected_tags)
 
