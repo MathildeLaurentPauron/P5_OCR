31c49dbe5a08c0c552a6cbee46bb9dbcf57f57a4
Mathilde Laurent Pauron <mathilde.pauron@gmail.com>
2025-01-17 22:39:28 +0100

    embedding modification

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
-# import tensorflow_hub as hub
-# import tensorflow
+import tensorflow_hub as hub
+import tensorflow
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer 
 import pickle
-import nltk
+import nltk 
 import re
 from nltk.corpus import stopwords
 nltk.download('stopwords')
@@ -16,27 +16,29 @@ def clean_text(text):
     text = re.sub(r'[^\w\s#+]', '', text)  # Enlever la ponctuation
     text = text.lower()  # Convertir en minuscules
     text = re.sub(r'<[^>]+>', '', text)  # Enlever les balises HTML
-    tokens = nltk.word_tokenize(text)  # Tokenisation
-    tokens = [word for word in tokens if word not in stopwords.words('english')]  # Suppression des stopwords
-    return ' '.join(tokens)
+    return text
 
 # Téléchargement et chargement du binariseur depuis MLflow
-binarizer_path = "mlb.pkl"
+binarizer_path = "mlb_use.pkl"
 with open(binarizer_path, "rb") as f:
     mlb = pickle.load(f)
 
-# Feature extraction with Bag-of-Words
-def get_bow_embedding(question):
-    vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
-    return vectorizer.transform([clean_text(question)]).toarray()
+# Charger le modèle USE depuis TensorFlow Hub
+use_model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+# Fonction pour obtenir l'embedding d'un texte avec USE
+def get_use_embedding(question):
+    return use_model([clean_text(question)]).numpy().flatten()
 
 # Charger le modèle
-model_path = "model_BoW.pkl"
+model_path = "model_use.pkl"
 with open(model_path, "rb") as f:
     model = pickle.load(f)
 
 def predict_tags(question):
-    embedding = get_bow_embedding(question)
+    embedding = get_use_embedding(question)
+    if embedding.ndim == 1:
+        embedding = embedding.reshape(1, -1)
     keywords = model.predict(embedding)
     predicted_tags = mlb.inverse_transform(keywords)
     return predicted_tags
