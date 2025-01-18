6c442b69f2db00eb06cd3babbb83fc72599109fb
Mathilde Laurent Pauron <mathilde.pauron@gmail.com>
2025-01-18 13:36:08 +0100

    final relecture before deployment

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import tensorflow
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import CountVectorizer 
 import pickle
 import nltk 
 import re
